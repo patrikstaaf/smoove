0b268a3065df883c521f95f6c7e963b8615637ea
patrikstaaf <staaf.patrik@gmail.com>
2021-11-18 00:00:30 +0100

    Minor updates to navbar, animation section and footer

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function setDimensions() {
   // Get the viewport height and multiple it by 1% to get a value for a vh unit
-  let vh = window.innerHeight * 0.01;
+  const vh = window.innerHeight * 0.01;
   // Set the value in the --vh custom property to the root of the document
   document.documentElement.style.setProperty("--vh", `${vh}px`);
 }
