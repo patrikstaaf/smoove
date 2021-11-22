735c80511a38a8fafe6f3f0b80ab95897cc7d7be
sofiaronnkvist <89849180+sofiaronnkvist@users.noreply.github.com>
2021-11-22 14:26:58 +0100

    Added new navbar with dropdown.

--- script.js
+++ script.js
@@ -8,15 +8,40 @@ function setDimensions() {
 setDimensions();
 window.addEventListener("resize", setDimensions);
 
+/* Newsletter dropdown */
+
 const exitNewsletter = document.querySelector(".exitNewsletter");
 const newsletter = document.querySelector(".newsletter-dropdown");
-const navbar = document.querySelector(".menu");
+const navbar = document.querySelector(".navbar");
 const open = document.querySelector(".open");
+const dropdownMenu = document.querySelector(".nav-menu");
 
 const hideNewsletter = exitNewsletter.addEventListener(
   "click",
   function (event) {
     newsletter.style.display = "none";
     navbar.style.marginTop = 0;
+    dropdownMenu.style.top = 64 + "px";
   }
 );
+
+/* Navigation */
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
