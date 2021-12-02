ce5427f28f8e363b17fd9c89568e25ef3d9918ce
sofiaronnkvist <89849180+sofiaronnkvist@users.noreply.github.com>
2021-12-02 17:39:49 +0100

    Removed js.

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// function setDimensions() {
-//   // Get the viewport height and multiple it by 1% to get a value for a vh unit
-//   const vh = window.innerHeight * 0.01;
-//   // Set the value in the --vh custom property to the root of the document
-//   document.documentElement.style.setProperty("--vh", `${vh}px`);
-// }
-
-// setDimensions();
-// window.addEventListener("resize", setDimensions);
-
 /* Get nav height for anchor links */
 
 const setNavHeight = function () {
